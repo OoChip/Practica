f1500280ecab74f11e69bbf28ae71d350d15fef5
OoChip <oochip2001@gmail.com>
2022-02-10 08:23:56 -0500

    add fizzbuzz

--- app.js
+++ app.js
@@ -38,3 +38,20 @@ function lastletter(str) {
 console.log (lastletter("guanabana"))
 console.log (lastletter("platano"))
 console.log (lastletter("tomate"))
+
+console.log ("Iniciando FizzBuzz")
+
+let input= parseInt(prompt("Introduzca un numero entero positivo"))
+
+for (let i = 1; i <= input; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log ("FizzBuzz")
+    } else if (i % 5 === 0) {  
+        console.log ("Buzz")
+    } else if (i % 3 === 0){
+        console.log ("Fizz")
+    } else {
+        console.log (i)
+    }
+
+}
